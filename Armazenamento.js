37e0ae327c478f5f50925d3ac06f26d9241983de
ANNA CLARA RODRIGUES REBECCA <anna.rebecca@portalsesisp.org.br>
2026-03-11 08:53:12 -0300

    Commit incial: Estrutura do projeto

---
+++ Armazenamento.js
@@ -0,0 +1,5 @@
+let nome = "Carlos";
+let idade = 25;
+
+console.log("nome: "+ nome);
+console.log("idade: "+ idade);
---
+++ escopo.js
@@ -0,0 +1,10 @@
+function exemplo() {
+    let numero = 10;
+
+    if (numero > 5) {
+        let mensagem = "O número é maior que 5";
+        console.log(mensagem); //Acessível dentro do bloco if
+    }
+    console.log(numero);
+}
+exemplo();
