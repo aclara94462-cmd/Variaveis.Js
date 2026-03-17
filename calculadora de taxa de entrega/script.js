410201723c85dcb58c37c774bcb626c29071aa37
aclara94462-cmd <aclara94462@gmail.com>
2026-03-17 15:22:32 -0300

    Atividades

---
+++ calculadora de taxa de entrega/script.js
@@ -0,0 +1,11 @@
+const PRECO_PRODUTO = 30
+const TAXA_ENTREGA = 5
+
+function calcular(){
+let quantidadeInput = document.getElementById("quantidade").value
+let TotalProdutos = quantidadeInput + PRECO_PRODUTO
+let TotalEntrega = totalProdutos + TAXA_ENTREGA
+
+document.getElementById("resultado").innerHTML =
+"Valor total do pedido é: R$ " = totalPedido
+}
